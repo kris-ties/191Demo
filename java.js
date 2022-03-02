d50ca3c120cd2bb6b127ee5d91ef14bc32c77963
Kristine Nguyen <44600693+kris-ties@users.noreply.github.com>
2022-03-02 15:05:41 -0800

    Add files via upload

---
+++ java.js
@@ -0,0 +1,60 @@
+//project name
+function project_name() {
+    var project_name = document.getElementById("project_name").value;
+  }
+
+//sponsor name
+function sponsor_name() {
+    var sponsor_name = document.getElementById("sponsor_name").value;
+  }
+
+//class
+function class_name() {
+    var class_name = document.getElementById("class_name").value;
+  }
+
+//quarter
+function quarter() {
+    var quarter = document.getElementById("quarter").value;
+  }
+
+//sponsor team
+function sponsor_team() {
+    var sponsor_team = document.getElementById("sponsor_team").value;
+  }
+
+//notes
+function notes() {
+    var notes = document.getElementById("notes").value;
+  }
+
+//create button
+function create() {
+    document.getElementById("create").submit();
+}
+
+//create sponsor BUTTON
+function create_sponsor() {
+    location.href(create_sponsor.html);
+}
+
+//attach file?
+function file() {
+    document.getElementById("file").value;
+}
+
+//new sponsor name
+function new_sponsor_name() {
+    var new_sponsor_name = document.getElementById("new_sponsor_name").value;
+  }
+
+//new sponsor team
+function new_sponsor_team() {
+    var new_sponsor_team = document.getElementById("new_sponsor_team").value;
+  }
+
+// website url
+function url() {
+    var url = document.getElementById("url").value;
+  }
+
